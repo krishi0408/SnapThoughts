574a0e59f5de16ce4da1f3404066315a24547a41
Krishika Singh <krishika.singh@harness.io>
2023-11-19 01:45:36 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,9 @@
+// Import mongoose library for MongoDB interactions
 const mongoose = require('mongoose');
+
+// disabling strict mode for queries to allow flexibility
 mongoose.set('strictQuery', false);
+// asynchronous function to connect to MongoDB instances
 const connectDB = async() => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
